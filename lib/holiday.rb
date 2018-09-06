1acf0c0b4e15893e9acd5d8694379e2f24397468
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-06 19:58:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    season.to_s
+    stringed_season = season.to_s
+    stringed_season
     binding.pry
   end
 
