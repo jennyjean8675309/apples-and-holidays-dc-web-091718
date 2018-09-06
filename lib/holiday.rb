e661ac17102b22d6f589ea6339e207d29b1e6cff
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-06 20:06:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,6 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
     stringed_season.capitalize!
     holiday_hash[season].each do |holiday, supplies|
       stringed_holiday = holiday.to_s
+    end
     binding.pry
   end
 
